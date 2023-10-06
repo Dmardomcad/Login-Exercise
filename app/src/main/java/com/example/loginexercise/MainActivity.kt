0ff0c41e8ab414d537bebb2b9ebec0cae08a0cc6
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-10-06 13:06:12 +0200

    Basic layout finished

--- app/src/main/java/com/example/loginexercise/MainActivity.kt
+++ app/src/main/java/com/example/loginexercise/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.loginexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.loginexercise.databinding.ActivityLoginBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginBtnClose.setOnClickListener{ closeApp() }
     }
+    private fun closeApp(){
+        finish()
+    }
+
+    data class User(
+        val username: String,
+        val email: String,
+        val password: String,
+    )
+
+    val mockUsers = listOf(
+        User("Pepe","pepegzlez@example.es","a1234567"),
+        User("shadowdarkness09","edgyemail@example.es","darks234")
+    )
+
 }

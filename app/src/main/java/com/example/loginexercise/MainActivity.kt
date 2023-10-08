61fc4dbc7937f236c56f555d7c466cd7081c599f
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-10-08 17:30:42 +0200

    Layout fixes and starting validations

--- app/src/main/java/com/example/loginexercise/MainActivity.kt
+++ app/src/main/java/com/example/loginexercise/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.loginexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import com.example.loginexercise.databinding.ActivityLoginBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.loginBtnClose.setOnClickListener{ closeApp() }
+        emailFocusListener()
+        passwordFocusListener()
     }
     private fun closeApp(){
         finish()
@@ -28,4 +31,39 @@ class MainActivity : AppCompatActivity() {
         User("shadowdarkness09","edgyemail@example.es","darks234")
     )
 
+    private fun emailFocusListener(){
+        binding.loginInputEmail.setOnFocusChangeListener {_, focused ->
+            if (!focused){
+                binding.loginContainerEmail.helperText = validEmail()
+            }
+        }
+    }
+    private fun validEmail(): String?{
+        val emailText = binding.loginInputEmail.text.toString()
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            return "Email no válido"
+        }
+
+        return null
+    }
+
+    private fun passwordFocusListener(){
+        binding.loginInputPassword.setOnFocusChangeListener { _, focused ->
+            if(!focused){
+                binding.loginContainerPassword.helperText = validPassword()
+            }
+        }
+    }
+    private fun validPassword(): String?{
+        val passwordText = binding.loginInputPassword.text.toString()
+        if(passwordText.length < 6){
+            return "Mínimo 6 carácteres"
+        }
+        if(!passwordText.matches(".*[A-Z].*".toRegex())){
+            return "Debe tener al menos 1 mayúscula"
+        }
+        //To do rest of the validations...
+
+        return null
+    }
 }
